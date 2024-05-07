35ea02951e37bb1aab20212cd48549a9e5f60060
Victor Decleire <vdecleir@shi-c1-p3.s19.be>
2024-05-07 14:13:17 +0200

    skull

---
+++ include/minishell.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdecleir <vdecleir@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/07 14:08:02 by vdecleir          #+#    #+#             */
+/*   Updated: 2024/05/07 14:09:55 by vdecleir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+
+
+#endif
