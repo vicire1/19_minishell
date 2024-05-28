841e3e8258f792efd15e1f9d57182b94f5b06204
Lbirlouet <lucasbirlouet@gmail.com>
2024-05-28 12:56:46 +0200

    replace every var so need to fix quotes counter and tej quotes

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -30,62 +30,70 @@ char	*in_env(t_data *data, t_env *env, char *str)
 int	valid_quotes_env(t_data *data, t_lexer *exp, int i)
 {
 	int	j;
-	int	doll;
+	// int	tempo_j = 0;
 	char *tempo_val;
 	char *tempo;
 	int	val_len;
 	int	name_len;
-
+	char *first_part;
+	char *sec_part;
+	char *third_part;
+(void)i;
 	j = 0;
-	doll = -1;
 	val_len = 0;
 	name_len = 0;
 	while (exp->token_str[j])
 	{
 		if (exp->token_str[j] == '$')
 		{
-			doll++;
-			if (doll == i)
+			if (count_quotes(data, exp, j))
 			{
-				if (count_quotes(data, exp, j))
+				printf("VALID_QUOTES_FAUT REPLACE [$%c]\n", exp->token_str[j+1]);
+				tempo_val = in_env(data, data->first_env, exp->token_str + j + 1);
+				printf("[1] [%s]\n", tempo_val);
+				name_len = size_env_doll(exp->token_str + j + 1);
+				printf("[2] [%d]\n", name_len);
+				val_len = 0;
+				if (tempo_val)
 				{
-					printf("VALID_QUOTES_FAUT REPLACE [$%c]\n", exp->token_str[j+1]);
-					tempo_val = in_env(data, data->first_env, exp->token_str + j + 1);
-					name_len = size_env_doll(exp->token_str + j);
-					if (tempo_val)
-					{
-						val_len = size_env_value(tempo_val);
-						name_len = size_env_name(tempo_val);
-						printf("EXISTE => %d\n", val_len);
-					}
-					printf("[%s][%d]\n", tempo_val, j);
-					printf("RETIRER %d et AJT %d donc realloc de \n", name_len, val_len);
-					tempo = ft_strdup(exp->token_str);
-					if (!tempo)
-						free_all(data, ERR_MAL, 1);
-					exp->token_str = realloc(exp->token_str, (ft_strlen(tempo) - name_len + val_len));
-					// exp->token_str = NULL;
-					printf("======EVOLUTION======\n");
-					printf("LENS => val_len = %d | name_len = %d | j = %d\n", val_len, name_len, j);
-					strncpy(exp->token_str, tempo, j);
-					printf("[1] %s\n", exp->token_str);
-					strcpy(exp->token_str + j, tempo_val+name_len);
-					printf("[2] %s\n", exp->token_str);
-					strcpy(exp->token_str + j + val_len, tempo + val_len + 1 );
-					printf("[3] %s\n", exp->token_str);
-					free(tempo);
-					printf("(%s)\n", exp->token_str);
-					//faire avec des joins !!!!!!!!!!!!!!!!!!!!!!
-					//faire une seule boucle (en quelques sorte delete celle de la fonction precedente), des que je crois un doll je m'en occupe direct, puis je pass au suivant.
-					//pour les quotes, je gere ca tout a la fin
-					// copier jusqua j, copine tempo puis copier la fin
-
-					return (1);
+					val_len = size_env_value(tempo_val);
+				printf("[3] [%d]\n", val_len);
+					name_len = size_env_name(tempo_val);
+				printf("[4] [%d]\n", name_len);
 				}
+				tempo = ft_strdup(exp->token_str);
+				printf("[5] [%s]\n", tempo);
+				if (!tempo)
+					free_all(data, ERR_MAL, 1);
+				first_part = ft_substr(tempo, 0, j, data);
+				printf("[6] [%s]\n", first_part);
+				sec_part = ft_substr(tempo_val, name_len, val_len, data);
+				printf("[7] [%s]\n", sec_part);
+				if (sec_part)
+				{
+					third_part = ft_substr(tempo, j + name_len, strlen(tempo), data);
+					printf("[8] [%s]\n", third_part);
+				}
+				else
+				{
+					third_part = ft_substr(tempo, j + name_len + 1, strlen(tempo), data);
+					printf("[8] [%s]\n", third_part);
+				}
+				free(exp->token_str);
+				free(tempo);
+				tempo = ft_strjoin(first_part, sec_part);
+				printf("[9] [%s]\n", tempo);
+				if (tempo)
+					exp->token_str = ft_strjoin(tempo , third_part);
+				else
+					exp->token_str = ft_strjoin(first_part , third_part);
+				free(tempo);
 			}
+			
 		}
 		j++;
 	}
+	printf("STRING FINALE |%s|\n", exp->token_str);
 	return (0);
 }
 
@@ -95,16 +103,17 @@ void	env_str(t_data *data, t_lexer	*exp)
 
 	int	j = 0;
 	int i = 0;
-	int nb_env = check_v_env(exp->token_str);
-	while (i < nb_env)
-	{
+	// int nb_env = check_v_env(exp->token_str);
+	// while (i < nb_env)
+	// {
 		j = valid_quotes_env(data, exp, i);
+		
 		// if (j == 1)
 		// 	printf("cas db quotes\n");
 		// else if (j == 2)
 		// 	printf("cas singles quotes\n");
-		i++;
-	}
+		// i++;
+	// }
 	return;
 }
 
