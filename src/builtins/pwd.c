02e1d7800b529ef222b261cd9255f1df5e8c5e44
Lbirlouet <lucasbirlouet@gmail.com>
2024-06-13 08:57:20 +0200

    protec pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -5,11 +5,27 @@
 //utiliser getcwd
 void	cmd_pwd(t_data *data, int fd)
 {
-	(void)data;
 	char	*pwd;
+	t_env	*tempo;
 
+	(void)data;
+	tempo = data->first_env;
 	pwd = getcwd(NULL, 0);
-	ft_printf_fd(fd, "%s\n", pwd);
-	free(pwd);
+	if (pwd)
+		ft_printf_fd(fd, "%s\n", pwd);
+	else
+	{
+		while (tempo)
+		{
+			if (ft_strncmp(tempo->env_str, "PWD=", 4) == 0)
+			{
+				ft_printf_fd(fd, "%s\n", tempo->env_str + 4);
+				break ;
+			}
+			tempo = tempo->next;
+		}
+	}
+	if (pwd)
+		free(pwd);
 	return ;
 }
