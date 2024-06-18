1ade0fd7076fd6c5a6d819837242b46dd481850e
Lbirlouet <lucasbirlouet@gmail.com>
2024-06-18 12:34:42 +0200

    export without arg print in ascii order

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -1,38 +1,84 @@
 #include "../../include/minishell.h"
 
-int	cmd_export_check_ascii(t_data *data)
+void	sort_tab(t_env **arr, int n)
 {
-	t_env *temp;
-	int	check;
-	// int	check2;
+	t_env	*key;
+	int		j;
+	int		i;
 
-	temp = data->first_env;
-	while (temp)
+	i = 1;
+	while (i < n)
 	{
-		check = (int)temp->name[0];
-		temp = temp->next;
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && strcmp(arr[j]->name, key->name) > 0)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+		i++;
 	}
-	return (0);
 }
 
+int	lst_env_size(t_data *data)
+{
+	t_env	*tempo;
+	int		ret;
 
+	tempo = data->first_env;
+	ret = 0;
+	while (tempo)
+	{
+		ret++;
+		tempo = tempo->next;
+	}
+	return (ret);
+}
+
+void	print_export(t_data *data, int count, t_env **env_array, int fd)
+{
+	t_env *tempo;
+	int		i;
+
+	tempo = data->first_env;
+	i = 0;
+	while (i < count)
+	{
+		tempo = env_array[i];
+		if (tempo->value)
+			ft_printf_fd(fd, "declare -x %s\"%s\"\n",
+				tempo->name, tempo->value);
+		else if (tempo->name[ft_strlen(tempo->name) - 1] == '=')
+			ft_printf_fd(fd, "declare -x %s\"\"\n", tempo->name);
+		else
+			ft_printf_fd(fd, "declare -x %s\n", tempo->name);
+		i++;
+	}
+	return ;
+}
 
 void	cmd_export_print(t_data *data, int fd)
 {
 	t_env	*temp;
+	t_env	**env_array;
+	int		count;
+	int		i;
 
+	i = 0;
+	count = lst_env_size(data);
+	env_array = (t_env **)malloc(count * sizeof(t_env *));
+	if (!env_array)
+		free_all(data, ERR_MAL, 0);
 	temp = data->first_env;
 	while (temp)
 	{
-		if (temp->value)
-			ft_printf_fd(fd, "declare -x %s\"%s\"\n", temp->name, temp->value);
-		else if (temp->name[ft_strlen(temp->name) - 1] == '=')
-			ft_printf_fd(fd, "declare -x %s\"\"\n", temp->name);
-		else
-			ft_printf_fd(fd, "declare -x %s\n", temp->name);
+		env_array[i++] = temp;
 		temp = temp->next;
 	}
-	return ;
+	sort_tab(env_array, count);
+	print_export(data, count, env_array, fd);
+	free(env_array);
 }
 
 void	cmd_export(t_data *data, char **str, int fd)
